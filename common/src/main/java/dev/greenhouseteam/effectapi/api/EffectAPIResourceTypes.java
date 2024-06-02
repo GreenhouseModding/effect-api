f8a07b2989819a4726e356ecff48588069aba002
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-06-03 03:10:58 +1000

    Add more resource types.

--- common/src/main/java/dev/greenhouseteam/effectapi/api/EffectAPIResourceTypes.java
+++ common/src/main/java/dev/greenhouseteam/effectapi/api/EffectAPIResourceTypes.java
@@ -1,18 +1,17 @@
 package dev.greenhouseteam.effectapi.api;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
-import dev.greenhouseteam.effectapi.api.effect.ResourceEffect;
 import dev.greenhouseteam.effectapi.impl.EffectAPI;
 import dev.greenhouseteam.effectapi.api.registry.EffectAPIRegistries;
 import dev.greenhouseteam.effectapi.impl.registry.internal.RegistrationCallback;
-import net.minecraft.core.component.DataComponentType;
-
-import java.util.List;
 
 public class EffectAPIResourceTypes {
 
     public static void registerAll(RegistrationCallback<Codec<?>> callback) {
+        callback.register(EffectAPIRegistries.RESOURCE_TYPE, EffectAPI.asResource("bool"), Codec.BOOL);
+        callback.register(EffectAPIRegistries.RESOURCE_TYPE, EffectAPI.asResource("double"), Codec.DOUBLE);
+        callback.register(EffectAPIRegistries.RESOURCE_TYPE, EffectAPI.asResource("float"), Codec.FLOAT);
         callback.register(EffectAPIRegistries.RESOURCE_TYPE, EffectAPI.asResource("int"), Codec.INT);
+        callback.register(EffectAPIRegistries.RESOURCE_TYPE, EffectAPI.asResource("string"), Codec.STRING);
     }
 }
