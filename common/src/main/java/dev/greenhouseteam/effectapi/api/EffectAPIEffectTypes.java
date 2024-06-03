83d2021257524fd4c17003660815871c0973e62e
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-06-04 03:23:02 +1000

    Add `trigger` effect type.

--- common/src/main/java/dev/greenhouseteam/effectapi/api/EffectAPIEffectTypes.java
+++ common/src/main/java/dev/greenhouseteam/effectapi/api/EffectAPIEffectTypes.java
@@ -1,10 +1,7 @@
 package dev.greenhouseteam.effectapi.api;
 
 import com.mojang.serialization.Codec;
-import dev.greenhouseteam.effectapi.api.effect.AttributeEffect;
-import dev.greenhouseteam.effectapi.api.effect.EffectAPIConditionalEffect;
-import dev.greenhouseteam.effectapi.api.effect.EntityTickEffect;
-import dev.greenhouseteam.effectapi.api.effect.ResourceEffect;
+import dev.greenhouseteam.effectapi.api.effect.*;
 import dev.greenhouseteam.effectapi.api.registry.EffectAPILootContextParamSets;
 import dev.greenhouseteam.effectapi.impl.EffectAPI;
 import dev.greenhouseteam.effectapi.api.registry.EffectAPIRegistries;
@@ -20,16 +17,20 @@ public class EffectAPIEffectTypes {
 
     public static final DataComponentType<List<EffectAPIConditionalEffect<AttributeEffect>>> ATTRIBUTE = DataComponentType.<List<EffectAPIConditionalEffect<AttributeEffect>>>builder()           .persistent(EffectAPIConditionalEffect.codec(AttributeEffect.CODEC, EffectAPILootContextParamSets.ENTITY).listOf())
             .build();
+    public static final DataComponentType<List<EffectAPIConditionalEffect<EntityTickEffect<?>>>> ENTITY_TICK = DataComponentType.<List<EffectAPIConditionalEffect<EntityTickEffect<?>>>>builder()
+            .persistent(EffectAPIConditionalEffect.codec(EntityTickEffect.CODEC, EffectAPILootContextParamSets.ENTITY).listOf())
+            .build();
     public static final DataComponentType<List<ResourceEffect<?>>> RESOURCE = DataComponentType.<List<ResourceEffect<?>>>builder()
             .persistent(ResourceEffect.CODEC.listOf())
             .build();
-    public static final DataComponentType<List<EffectAPIConditionalEffect<EntityTickEffect<?>>>> ENTITY_TICK = DataComponentType.<List<EffectAPIConditionalEffect<EntityTickEffect<?>>>>builder()
-            .persistent(EffectAPIConditionalEffect.codec(EntityTickEffect.CODEC, EffectAPILootContextParamSets.ENTITY).listOf())
+    public static final DataComponentType<List<EffectAPIConditionalEffect<TriggerEffect>>> TRIGGER = DataComponentType.<List<EffectAPIConditionalEffect<TriggerEffect>>>builder()
+            .persistent(EffectAPIConditionalEffect.codec(TriggerEffect.CODEC, EffectAPILootContextParamSets.ENTITY).listOf())
             .build();
 
     public static void registerAll(RegistrationCallback<DataComponentType<?>> callback) {
         callback.register(EffectAPIRegistries.EFFECT_TYPE, EffectAPI.asResource("attribute"), ATTRIBUTE);
         callback.register(EffectAPIRegistries.EFFECT_TYPE, EffectAPI.asResource("entity_tick"), ENTITY_TICK);
         callback.register(EffectAPIRegistries.EFFECT_TYPE, EffectAPI.asResource("resource"), RESOURCE);
+        callback.register(EffectAPIRegistries.EFFECT_TYPE, EffectAPI.asResource("trigger"), TRIGGER);
     }
 }
