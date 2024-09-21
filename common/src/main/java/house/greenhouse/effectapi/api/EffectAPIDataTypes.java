ebb65fc384bf8702c0f0696f55dfaf56ddbb298f
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2024-09-21 18:01:33 +1000

    Fix argument types having no entry point.

--- common/src/main/java/house/greenhouse/effectapi/api/EffectAPIDataTypes.java
+++ common/src/main/java/house/greenhouse/effectapi/api/EffectAPIDataTypes.java
@@ -6,13 +6,12 @@ import house.greenhouse.effectapi.api.registry.EffectAPIRegistries;
 import house.greenhouse.effectapi.api.variable.DataType;
 import house.greenhouse.effectapi.impl.EffectAPI;
 import house.greenhouse.effectapi.impl.registry.internal.RegistrationCallback;
-import net.minecraft.resources.ResourceLocation;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class EffectAPIDataTypes {
-    private static final Map<ResourceLocation, ArgumentType<?>> ARGUMENT_TYPES = new HashMap<>();
+    private static final Map<DataType<?>, ArgumentType<?>> ARGUMENT_TYPES = new HashMap<>();
 
     public static final DataType<Boolean> BOOL = new DataType<>(Codec.BOOL, false);
     public static final DataType<Double> DOUBLE = new DataType<>(Codec.DOUBLE, 0.0D);
@@ -30,18 +29,23 @@ public class EffectAPIDataTypes {
         registerArgumentTypes();
     }
 
-    public static <T> ArgumentType<T> getArgumentType(DataType<T> dataType) {
-        ResourceLocation id = EffectAPIRegistries.DATA_TYPE.getKey(dataType);
-        if (!ARGUMENT_TYPES.containsKey(id))
-            throw new NullPointerException("Could not find argument type for resource type '" + id + "'. Please check that an associated argument type has been registered.");
-        return (ArgumentType<T>) ARGUMENT_TYPES.get(id);
+    public static void registerArgumentTypes() {
+        registerArgumentType(BOOL, BoolArgumentType.bool());
+        registerArgumentType(DOUBLE, DoubleArgumentType.doubleArg());
+        registerArgumentType(FLOAT, FloatArgumentType.floatArg());
+        registerArgumentType(INT, IntegerArgumentType.integer());
+        registerArgumentType(STRING, StringArgumentType.string());
     }
 
-    public static void registerArgumentTypes() {
-        ARGUMENT_TYPES.put(EffectAPI.asResource("bool"), BoolArgumentType.bool());
-        ARGUMENT_TYPES.put(EffectAPI.asResource("double"), DoubleArgumentType.doubleArg());
-        ARGUMENT_TYPES.put(EffectAPI.asResource("float"), FloatArgumentType.floatArg());
-        ARGUMENT_TYPES.put(EffectAPI.asResource("int"), IntegerArgumentType.integer());
-        ARGUMENT_TYPES.put(EffectAPI.asResource("string"), StringArgumentType.string());
+    public static <T> void registerArgumentType(DataType<?> dataType, ArgumentType<T> argumentType) {
+        if (ARGUMENT_TYPES.containsKey(dataType))
+            throw new UnsupportedOperationException("Data type \"" + EffectAPIRegistries.DATA_TYPE.getKey(dataType) + "\" has not been registered.");
+        ARGUMENT_TYPES.put(dataType, argumentType);
+    }
+
+    public static <T> ArgumentType<T> getArgumentType(DataType<T> dataType) {
+        if (!ARGUMENT_TYPES.containsKey(dataType))
+            throw new NullPointerException("Could not find argument type for resource type \"" + EffectAPIRegistries.DATA_TYPE.getKey(dataType) + "\". Please check that an associated argument type has been registered.");
+        return (ArgumentType<T>) ARGUMENT_TYPES.get(dataType);
     }
 }
